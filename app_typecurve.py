9d9495aa17dd38e7af134fc61ea77a3dd75db0b1
VO THANH HUNG <33250137+kelvin-hung@users.noreply.github.com>
2025-12-04 09:23:45 +0900

    Update app_typecurve.py

--- app_typecurve.py
+++ app_typecurve.py
@@ -21,11 +21,11 @@ st.markdown(
 # ----------------- INPUT SIDEBAR -----------------
 st.sidebar.header("Reservoir & operation inputs")
 
-phi   = st.sidebar.slider("Porosity ϕ [-]", 0.05, 0.35, 0.20, 0.01)
-kh    = st.sidebar.slider("kh [mD]", 1.0, 1000.0, 150.0, 1.0)
+phi   = st.sidebar.slider("Porosity ϕ [-]", 0.05, 0.4, 0.20, 0.01)
+kh    = st.sidebar.slider("kh [mD]", 1.0, 2000.0, 150.0, 1.0)
 kvkh  = st.sidebar.slider("kv/kh [-]", 0.05, 1.0, 0.5, 0.01)
 h_m   = st.sidebar.slider("Net thickness h [m]", 10.0, 300.0, 120.0, 5.0)
-qinj  = st.sidebar.slider("q_inj [10³ Sm³/d]", 25.0, 1000.0, 600.0, 25.0)
+qinj  = st.sidebar.slider("q_inj [10³ Sm³/d]", 25.0, 10000.0, 600.0, 25.0)
 cr = st.slider("Rock compressibility [1/psi]", 
                min_value=1e-6, 
                max_value=2e-5, 
@@ -301,4 +301,5 @@ plt.savefig("fig_typecurve_components_physical_units.png", dpi=300)
 plt.close()
 
 print("Saved: fig_typecurve_components_physical_units.png")
+
 
